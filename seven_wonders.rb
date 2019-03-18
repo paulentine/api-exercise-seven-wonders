f66aa4dd5310db0f13b8b4ea2f3bcc04cd029b0f
Pauline Sauget <paulinevsauget@gmail.com>
2019-03-18 14:21:01 -0700

    Wrote code to get locations of seven wonders from API

--- seven_wonders.rb
+++ seven_wonders.rb
@@ -3,20 +3,25 @@ require 'httparty'
 #Starter Code:
 seven_wonders = ["Great Pyramid of Giza", "Hanging Gardens of Babylon", "Colossus of Rhodes", "Pharos of Alexandria", "Statue of Zeus at Olympia", "Temple of Artemis", "Mausoleum at Halicarnassus"]
 
-BASE_URL = "THE BASE URL FOR THE API REQUEST"
-key = "YOUR API TOKEN"
+BASE_URL = 'https://us1.locationiq.com/v1/search.php'
+
+seven_wonders_hash = Hash.new(0)
 
 seven_wonders_locations = seven_wonders.map do |wonder|
-  # Code to discover the locations of each wonder.
-  
+  query_parameters = {
+    key: 'b1bb297e767eac',
+    q: wonder,
+    format: 'json'
+  }
+  sleep(1)
+  response = HTTParty.get(BASE_URL, query: query_parameters)[0]
+  seven_wonders_hash[wonder] = {
+    "lat" => response["lat"],
+    "lng" => response["lon"]
+  }
 end
 
-
-
-
-
-
-
+puts seven_wonders_hash
 
 #Example Output:
 #{"Great Pyramind of Giza"=>{"lat"=>29.9792345, "lng"=>31.1342019}, "Hanging Gardens of Babylon"=>{"lat"=>32.5422374, "lng"=>44.42103609999999}, "Colossus of Rhodes"=>{"lat"=>36.45106560000001, "lng"=>28.2258333}, "Pharos of Alexandria"=>{"lat"=>38.7904054, "lng"=>-77.040581}, "Statue of Zeus at Olympia"=>{"lat"=>37.6379375, "lng"=>21.6302601}, "Temple of Artemis"=>{"lat"=>37.9498715, "lng"=>27.3633807}, "Mausoleum at Halicarnassus"=>{"lat"=>37.038132, "lng"=>27.4243849}}
